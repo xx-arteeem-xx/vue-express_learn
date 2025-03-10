7cad22579ce053ebc3e77f91996efafbe669e3f1
Artem Firsov <artem.firsov.qwe@gmail.com>
2025-03-10 20:59:09 +0700

    adding backend part

---
+++ api/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const test_data = {
+    1: {
+        title: "test",
+        desc: "lorem ipsum"
+    },
+    2: {
+        title: "test2",
+        desc: "lorem ipsum2"
+    }
+};
+
+app.get('/', (req, res) => {
+  res.json(test_data);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
