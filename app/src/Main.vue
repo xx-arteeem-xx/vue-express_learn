bac4bf6eae9a6c33985f6a455850084e0ae2efc5
Artem Firsov <artem.firsov.qwe@gmail.com>
2025-03-11 23:58:01 +0700

    add a footer

--- app/src/Main.vue
+++ app/src/Main.vue
@@ -1,11 +1,27 @@
 <script setup>
     import Header from './components/Header.vue'
+    import Footer from './components/Footer.vue'
 </script>
 
 <template>
     <Header />
+
+    <div class="container my-5 main">
+        <div class="p-5 bg-body-tertiary rounded-3">
+            <h1 class="text-body-emphasis">
+                Notes Pro - сервис для заметок
+            </h1>
+            <p class="fs-5 text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iure qui tenetur consequatur minus explicabo ut ducimus dicta quia facere, in deserunt. Corrupti et iure obcaecati consequatur, at quidem fuga!
+            </p>
+        </div>
+    </div>
+
+    <Footer />
 </template>
 
 <style>
-
+    .main{
+        min-height: 50vh;
+    }
 </style>
---
+++ app/src/components/Footer.vue
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+
+<template>
+    <div class="container footer">
+        <div class="row align-items-center">
+            <div class="col-3 d-flex flex-column justify-content-center py-3">
+                <a href="/" class="link-secondary">
+                    Главная
+                </a>
+                <a href="/" class="link-secondary">
+                    О проекте
+                </a>
+                <a href="/" class="link-secondary">
+                    Мои заметки
+                </a>
+                <a href="/" class="link-secondary">
+                    Создать
+                </a>
+            </div>
+            <div class="col-5 d-flex flex-column justify-content-center py-3">
+                <a href="/" class="link-secondary">
+                    Политика конфиденциальности
+                </a>
+                <a href="/" class="link-secondary">
+                    Условия использования
+                </a>
+            </div>
+            <div class="col-4 text-center py-3">
+                <a href="/" class="btn">
+                    <h2>
+                        <i class="fa-solid fa-book"></i>
+                        Notes Pro
+                    </h2>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+    
+</style>
