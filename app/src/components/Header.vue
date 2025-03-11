9a9a527052411223e993b4fab1fc9274ffe5015e
Artem Firsov <artem.firsov.qwe@gmail.com>
2025-03-11 23:39:50 +0700

    edit header 1

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -5,28 +5,30 @@
 <template>
     <div class="row align-items-center">
         <div class="col-3 text-center py-3">
-            <h2>
-                <i class="fa-solid fa-book"></i>
-                Notes Pro
-            </h2>
+            <a href="/" class="btn">
+                <h2>
+                    <i class="fa-solid fa-book"></i>
+                    Notes Pro
+                </h2>
+            </a>
         </div>
         <div class="col-5 d-flex align-items-center justify-content-center gap-3 py-3">
-            <button class="btn btn-outline-secondary">
+            <a href="/" class="btn btn-outline-secondary">
                 <i class="fa-solid fa-house"></i>
                 Главная
-            </button>
-            <button class="btn btn-outline-secondary">
+            </a>
+            <a href="/" class="btn btn-outline-secondary">
                 <i class="fa-solid fa-circle-info"></i>
                 О проекте
-            </button>
-            <button class="btn btn-outline-secondary">
+            </a>
+            <a href="/" class="btn btn-outline-secondary">
                 <i class="fa-solid fa-note-sticky"></i>
                 Мои заметки
-            </button>
-            <button class="btn btn-outline-secondary">
+            </a>
+            <a href="/" class="btn btn-outline-secondary">
                 <i class="fa-solid fa-circle-plus"></i>
                 Создать
-            </button>
+            </a>
         </div>
         <div class="col-4 d-flex align-items-center justify-content-center gap-3 py-3">
             <div class="dropdown-center">
@@ -38,14 +40,14 @@
                     <li><a class="dropdown-item" href="#">EN</a></li>
                 </ul>
             </div>
-            <button class="btn btn-outline-secondary">
+            <a href="/" class="btn btn-outline-secondary">
                 <i class="fa-solid fa-right-to-bracket"></i>
                 Войти
-            </button>
-            <button class="btn btn-outline-secondary">
+            </a>
+            <a href="/" class="btn btn-outline-secondary">
                 <i class="fa-solid fa-id-card"></i>
                 Зарегистрироваться
-            </button>
+            </a>
         </div>
     </div>
 </template>
