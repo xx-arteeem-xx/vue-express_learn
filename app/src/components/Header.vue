dd18a2213088f964e3cc8146636246f9d66d3316
Artem Firsov <artem.firsov.qwe@gmail.com>
2025-03-11 23:33:46 +0700

    add a header

---
+++ app/src/components/Header.vue
@@ -0,0 +1,55 @@
+<script>
+
+</script>
+
+<template>
+    <div class="row align-items-center">
+        <div class="col-3 text-center py-3">
+            <h2>
+                <i class="fa-solid fa-book"></i>
+                Notes Pro
+            </h2>
+        </div>
+        <div class="col-5 d-flex align-items-center justify-content-center gap-3 py-3">
+            <button class="btn btn-outline-secondary">
+                <i class="fa-solid fa-house"></i>
+                Главная
+            </button>
+            <button class="btn btn-outline-secondary">
+                <i class="fa-solid fa-circle-info"></i>
+                О проекте
+            </button>
+            <button class="btn btn-outline-secondary">
+                <i class="fa-solid fa-note-sticky"></i>
+                Мои заметки
+            </button>
+            <button class="btn btn-outline-secondary">
+                <i class="fa-solid fa-circle-plus"></i>
+                Создать
+            </button>
+        </div>
+        <div class="col-4 d-flex align-items-center justify-content-center gap-3 py-3">
+            <div class="dropdown-center">
+                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-language"></i>
+                </button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">RU</a></li>
+                    <li><a class="dropdown-item" href="#">EN</a></li>
+                </ul>
+            </div>
+            <button class="btn btn-outline-secondary">
+                <i class="fa-solid fa-right-to-bracket"></i>
+                Войти
+            </button>
+            <button class="btn btn-outline-secondary">
+                <i class="fa-solid fa-id-card"></i>
+                Зарегистрироваться
+            </button>
+        </div>
+    </div>
+</template>
+
+<style>
+    
+</style>
--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,4 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+import Main from './Main.vue'
 
-createApp(App).mount('#app')
+createApp(Main).mount('#app')
